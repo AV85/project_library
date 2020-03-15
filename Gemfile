a8f4ee26da9344ac8a2edb90f141722ea5e61c4a
Alexandr <sasha85ua@gmail.com>
2020-03-15 20:09:25 +0200

    added cloudenery

--- Gemfile
+++ Gemfile
@@ -24,6 +24,7 @@ gem 'sass-rails', '~> 5.0'
 gem 'turbolinks', '~> 5'
 gem 'uglifier', '>= 1.3.0'
 gem 'gravatar_image_tag'
+gem 'cloudinary'
 
 group :development, :test do
   gem 'byebug', platforms: %i[mri mingw x64_mingw]
---
+++ config/initializers/cloudinary.rb
@@ -0,0 +1,7 @@
+Cloudinary.config do |config|
+  config.cloud_name = 'av85'
+  config.api_key = '756825649582242'
+  config.api_secret = 'QijEJU_RWeTBppX7s5GR9O_C6Wg'
+  config.secure = true
+  config.cdn_subdomain = true
+end
